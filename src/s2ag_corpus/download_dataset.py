b2589ff85ad3a0ae004dbf6a0cf592afcfd16284
Romilly Cocking <romilly.cocking@gmail.com>
2024-04-15 06:37:52 +0100

    Tidied up dataset loader

--- src/s2ag_corpus/download_dataset.py
+++ src/s2ag_corpus/download_dataset.py
@@ -18,12 +18,16 @@ def download(release_id: str, dataset_name: str):
     download_links = download_links_response.json()["files"]
     print(f"about to download{release_id} of {dataset_name}: {len(download_links)} files.")
     for (i, link) in enumerate(download_links):
-        file_name = f"{base_dir}/{release_id}-{dataset_name}{i}.gz"
+        file_name = f"{dataset_name}{i}.gz"
         print(f"Downloading {file_name}")
         response = requests.get(link)
         if response.status_code != 200:
             break
-        with open(f"{base_dir}/{release_id}-{dataset_name}{i}.gz","wb") as pf:
+        file_path = f"{base_dir}/{release_id}/{dataset_name}/{file_name}.gz"
+        if os.path.exists(file_path):
+            print (f"skipping {file_name}")
+            continue
+        with open(file_path,"wb") as pf:
             pf.write(response.content)
 
 
