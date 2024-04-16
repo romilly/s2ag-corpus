7cb280d12390d53e84a2c3737b8e65c36387ec53
Romilly Cocking <romilly.cocking@gmail.com>
2024-04-16 15:02:36 +0100

    Preparing to load papers into production

--- src/load-production-corpus-papers.py
+++ src/load-production-corpus-papers.py
@@ -21,19 +21,18 @@ papers_dir = f"{base_dir}/{release_id}/papers/"
 for filename in sorted(os.listdir(papers_dir)):
     if filename.startswith("file"):
         print(f"processing: {filename}")
-        with open(base_dir + f"{papers_dir}{filename}") as f:
+        with open("{papers_dir}{filename}") as f:
             jason_dictionaries = [(line, json.loads(line)) for line in f.readlines()]
             records = [(jd['corpusid'], line.strip()) for  line, jd in jason_dictionaries]
-            with open(base_dir+'/2024-04-02/papers/transfer.csv','w') as csvf:
+            transfer_file = f'{papers_dir}/transfer.csv'
+            with open(transfer_file,'w') as csvf:
                 writer = csv.writer(csvf, delimiter=',', quoting=csv.QUOTE_NONE, escapechar='\\')
                 for record in records:
                     writer.writerow(record)
-
-        with open(base_dir+'/2024-04-02/papers/transfer.csv','r') as csvf:
+        with open(transfer_file,'r') as csvf:
             with connection.cursor() as cursor:
                 cursor.copy_from(csvf, 'papers', sep=',', null='')
             connection.commit()
-
 print('done')
 
 
