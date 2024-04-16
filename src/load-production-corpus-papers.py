60125716c818dc3c004d9575847395e2f23527a1
Romilly Cocking <romilly.cocking@gmail.com>
2024-04-16 15:36:09 +0100

    Fixing bugs to load papers into production

--- src/load-production-corpus-papers.py
+++ src/load-production-corpus-papers.py
@@ -18,21 +18,36 @@ catalogue = CorpusDatabaseCatalogue(connection)
 release_id = '2024-04-02'
 papers_dir = f"{base_dir}/{release_id}/papers/"
 
+def read_lines_from_file(file_path):
+    """A generator function that reads a file line by line."""
+    with open(file_path, 'r') as file:
+        for line in file:
+            yield line.strip()
+
+def read_lad_from_file(file_path):
+    for line in read_lines_from_file(file_path):
+        lad = (line, json.loads(line))
+        yield  lad
+
+def read_records_from_file(file_path):
+    for line, jd in read_lad_from_file(file_path):
+        record = (jd['corpusid'], line.strip())
+        yield record
+
+
 for filename in sorted(os.listdir(papers_dir)):
-    if filename.startswith("file"):
+    # if filename.startswith("file"):
+    if filename == "file":
         print(f"processing: {filename}")
-        with open(f"{papers_dir}/{filename}") as f:
-            jason_dictionaries = [(line, json.loads(line)) for line in f.readlines()]
-            records = [(jd['corpusid'], line.strip()) for  line, jd in jason_dictionaries]
-            transfer_file = f"{papers_dir}/transfer.csv"
-            with open(transfer_file,'w') as csvf:
-                writer = csv.writer(csvf, delimiter=',', quoting=csv.QUOTE_NONE, escapechar='\\')
-                count = 0
-                for record in records:
-                    writer.writerow(record)
-                    count += 1
-                    if 0 == count%10000:
-                        print(count)
+        transfer_file = f"{papers_dir}/transfer.csv"
+        with open(transfer_file,'w') as csvf:
+            writer = csv.writer(csvf, delimiter=',', quoting=csv.QUOTE_NONE, escapechar='\\')
+            count = 0
+            for record in read_records_from_file(f"{papers_dir}{filename}"):
+                writer.writerow(record)
+                count += 1
+                if 0 == count%10000:
+                    print(count)
         with open(transfer_file,'r') as csvf:
             with connection.cursor() as cursor:
                 cursor.copy_from(csvf, 'papers', sep=',', null='')
