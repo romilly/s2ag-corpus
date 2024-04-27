b2634edd60d9c24f1a13d0928673f184eb5a7ee6
Romilly Cocking <romilly.cocking@gmail.com>
2024-04-27 12:20:10 +0100

    Lots more refactoring. About to test production update on pi5-corpus

---
+++ src/load-production-corpus-citations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import os
+from dotenv import load_dotenv
+
+from s2ag_corpus.import_dataset import insert_dataset
+from s2ag_corpus.database_catalogue import production_connection
+
+
+load_dotenv()
+base_dir = os.getenv("BASE_DIR")
+
+
+connection = production_connection()
+release_id = '2024-04-02'
+
+datasets_dir = f"{base_dir}/{release_id}/"
+dataset_name = 'citations'
+
+insert_dataset(dataset_name, datasets_dir, connection)
+
