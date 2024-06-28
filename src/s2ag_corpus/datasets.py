b899bb3e0935045f32453046e55fe184b52a05f2
Romilly Cocking <romilly.cocking@gmail.com>
2024-06-28 09:50:14 +0100

    Starting work on diffs: upsert sql added to dataset definitions.

--- src/s2ag_corpus/datasets.py
+++ src/s2ag_corpus/datasets.py
@@ -1,13 +1,13 @@
 import json
 from dataclasses import dataclass
 
-from s2ag_corpus.sql import CREATE_ABSTRACTS_TABLE, ADD_KEYS_TO_ABSTRACTS
-from s2ag_corpus.sql import CREATE_AUTHORS_TABLE, ADD_KEYS_TO_AUTHORS
-from s2ag_corpus.sql import CREATE_CITATIONS_TABLE, ADD_KEYS_TO_CITATIONS
-from s2ag_corpus.sql import CREATE_PAPERS_TABLE, ADD_KEY_TO_PAPERS
-from s2ag_corpus.sql import CREATE_PAPER_IDS_TABLE, ADD_KEYS_TO_PAPER_IDS
-from s2ag_corpus.sql import CREATE_PUBLICATION_VENUES_TABLE, ADD_KEY_TO_PUBLICATION_VENUES
-from s2ag_corpus.sql import CREATE_TLDRS_TABLE, ADD_KEY_TO_TLDRS
+from s2ag_corpus.sql import CREATE_ABSTRACTS_TABLE, ADD_KEYS_TO_ABSTRACTS, UPSERT_ABSTRACTS
+from s2ag_corpus.sql import CREATE_AUTHORS_TABLE, ADD_KEYS_TO_AUTHORS, UPSERT_AUTHORS
+from s2ag_corpus.sql import CREATE_CITATIONS_TABLE, ADD_KEYS_TO_CITATIONS, UPSERT_CITATIONS
+from s2ag_corpus.sql import CREATE_PAPERS_TABLE, ADD_KEY_TO_PAPERS, UPSERT_PAPERS
+from s2ag_corpus.sql import CREATE_PAPER_IDS_TABLE, ADD_KEYS_TO_PAPER_IDS, UPSERT_PAPER_IDS
+from s2ag_corpus.sql import CREATE_PUBLICATION_VENUES_TABLE, ADD_KEY_TO_PUBLICATION_VENUES, UPSERT_PUBLICATION_VENUES
+from s2ag_corpus.sql import CREATE_TLDRS_TABLE, ADD_KEY_TO_TLDRS, UPSERT_TLDRS
 
 
 @dataclass(frozen=True)
@@ -16,11 +16,12 @@ class Dataset:
     json_to_tuple: callable
     create_table: str
     add_indices: str
+    upsert: str
 
 
 def abstract_json_to_tuple(line):
     jd = json.loads(line)
-    return jd['corpusid'], json.dumps(jd['openaccessinfo']),jd['abstract']
+    return jd['corpusid'], json.dumps(jd['openaccessinfo']), jd['abstract']
 
 
 def authors_json_to_tuple(line):
@@ -68,40 +69,51 @@ def tldrs_json_to_tuple(line):
 abstracts_dataset = Dataset(table="abstracts",
                             json_to_tuple=abstract_json_to_tuple,
                             create_table=CREATE_ABSTRACTS_TABLE,
-                            add_indices=ADD_KEYS_TO_ABSTRACTS)
+                            add_indices=ADD_KEYS_TO_ABSTRACTS,
+                            upsert=UPSERT_ABSTRACTS,
+                            )
 
 authors_dataset = Dataset(table="authors",
                           json_to_tuple=authors_json_to_tuple,
                           create_table=CREATE_AUTHORS_TABLE,
-                          add_indices=ADD_KEYS_TO_AUTHORS)
+                          add_indices=ADD_KEYS_TO_AUTHORS,
+                          upsert=UPSERT_AUTHORS,
+                          )
 
 citations_dataset = Dataset(table="citations",
                             json_to_tuple=citation_json_to_tuple,
                             create_table=CREATE_CITATIONS_TABLE,
-                            add_indices=ADD_KEYS_TO_CITATIONS
+                            add_indices=ADD_KEYS_TO_CITATIONS,
+                            upsert=UPSERT_CITATIONS,
                             )
 
 papers_dataset = Dataset(table="papers",
                          json_to_tuple=paper_json_to_tuple,
                          create_table= CREATE_PAPERS_TABLE,
-                         add_indices=ADD_KEY_TO_PAPERS
+                         add_indices=ADD_KEY_TO_PAPERS,
+                         upsert=UPSERT_PAPERS,
                          )
 
 paper_ids_dataset = Dataset(table="paperids",
                             json_to_tuple= paper_ids_json_to_tuple,
                             create_table=CREATE_PAPER_IDS_TABLE,
-                            add_indices=ADD_KEYS_TO_PAPER_IDS
+                            add_indices=ADD_KEYS_TO_PAPER_IDS,
+                            upsert=UPSERT_PAPER_IDS,
                             )
 
 publication_venues_dataset = Dataset(table="publication_venues",
                                      json_to_tuple= pv_json_to_tuple,
                                      create_table=CREATE_PUBLICATION_VENUES_TABLE,
-                                     add_indices=ADD_KEY_TO_PUBLICATION_VENUES)
+                                     add_indices=ADD_KEY_TO_PUBLICATION_VENUES,
+                                     upsert=UPSERT_PUBLICATION_VENUES,
+                                     )
 
 tldrs_dataset = Dataset(table="tldrs",
                         json_to_tuple=tldrs_json_to_tuple,
                         create_table=CREATE_TLDRS_TABLE,
-                        add_indices=ADD_KEY_TO_TLDRS)
+                        add_indices=ADD_KEY_TO_TLDRS,
+                        upsert=UPSERT_TLDRS,
+                        )
 
 DATASETS = {
     "abstracts": abstracts_dataset,
@@ -109,6 +121,6 @@ DATASETS = {
     "citations" : citations_dataset,
     "papers" : papers_dataset,
     "paper-ids" : paper_ids_dataset,
+    "publication-venues": publication_venues_dataset,
     "tldrs" : tldrs_dataset,
-    "publication-venues": publication_venues_dataset
 }
--- src/s2ag_corpus/sql.py
+++ src/s2ag_corpus/sql.py
@@ -12,6 +12,14 @@ ALTER TABLE abstracts
 ADD PRIMARY KEY (corpusid);
 """
 
+UPSERT_ABSTRACTS = """
+INSERT INTO abstracts (corpusid, openaccessinfo, abstract)
+VALUES (%s, %s, %s)
+ON CONFLICT (corpusid)
+DO UPDATE SET openaccessinfo = EXCLUDED.openaccessinfo,
+              abstract = EXCLUDED.abstract;
+"""
+
 CREATE_AUTHORS_TABLE = """
 create table authors (
     authorid bigint not null,
@@ -25,6 +33,14 @@ alter table public.authors
     add constraint authors_pk
         primary key (authorid);;
 create index name_idx on authors (name);
+"""
+
+UPSERT_AUTHORS = """
+INSERT INTO authors (authorid, name, author_json)
+VALUES (%s, %s, %s)
+ON CONFLICT (authorid)
+DO UPDATE SET name = EXCLUDED.name,
+              author_json = EXCLUDED.author_json;
 """
 
 
@@ -51,6 +67,17 @@ create index citations_citingcorpusid_index
     on citations (citingcorpusid);
 """
 
+UPSERT_CITATIONS = """
+INSERT INTO citations (citationid, citingcorpusid, citedcorpusid, isinfluential, contexts, intents)
+VALUES (%s, %s, %s, %s, %s, %s)
+ON CONFLICT (citationid)
+DO UPDATE SET citingcorpusid = EXCLUDED.citingcorpusid,
+              citedcorpusid = EXCLUDED.citedcorpusid,
+              isinfluential = EXCLUDED.isinfluential,
+              contexts = EXCLUDED.contexts,
+              intents = EXCLUDED.intents;
+"""
+
 CREATE_PAPERS_TABLE = """
 CREATE TABLE papers
     (corpusid int4  not null, paper_json jsonb not null)
@@ -60,6 +87,14 @@ ADD_KEY_TO_PAPERS = """
 alter table papers
     add constraint papers_pk
         primary key (corpusid);"""
+
+UPSERT_PAPERS = """
+INSERT INTO papers (corpusid, paper_json)
+VALUES (%s, %s)
+ON CONFLICT (corpusid)
+DO UPDATE SET paper_json = EXCLUDED.paper_json;
+"""
+
 
 CREATE_PAPER_IDS_TABLE = """
 create table public.paperids (
@@ -75,6 +110,14 @@ alter table paperids
 create index corpus_index on paperids(corpusid);
 """
 
+UPSERT_PAPER_IDS = """
+INSERT INTO paperids (sha, corpusid, is_primary)
+VALUES (%s, %s, %s)
+ON CONFLICT (sha)
+DO UPDATE SET corpusid = EXCLUDED.corpusid,
+              is_primary = EXCLUDED.is_primary;
+"""
+
 CREATE_PUBLICATION_VENUES_TABLE = """
 CREATE TABLE publication_venues (
     id text not null,
@@ -87,6 +130,14 @@ alter table publication_venues
         primary key (id)
 """
 
+UPSERT_PUBLICATION_VENUES = """
+INSERT INTO publication_venues (id, pv_json)
+VALUES (%s, %s)
+ON CONFLICT (id)
+DO UPDATE SET pv_json = EXCLUDED.pv_json;
+"""
+
+
 CREATE_TLDRS_TABLE = """
 CREATE TABLE tldrs
     (corpusid int4  not null, tldr_json jsonb not null)
@@ -96,4 +147,11 @@ ADD_KEY_TO_TLDRS = """
 alter table tldrs
  add constraint tldrs_pk
     primary key (corpusid);
+"""
+
+UPSERT_TLDRS = """
+INSERT INTO tldrs (corpusid, tldr_json)
+VALUES (%s, %s)
+ON CONFLICT (corpusid)
+DO UPDATE SET tldr_json = EXCLUDED.tldr_json;
 """
