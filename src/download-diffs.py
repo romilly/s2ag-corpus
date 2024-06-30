7fc85bf2943b096dcb1f8f04264e8f5b541fad18
Romilly Cocking <romilly.cocking@gmail.com>
2024-06-30 15:05:15 +0100

    Preparing for production run of diff code.

--- src/download-diffs.py
+++ src/download-diffs.py
@@ -1,16 +1,7 @@
-import os
-
-from dotenv import load_dotenv
-
 from s2ag_corpus.datasets import DATASETS
 from s2ag_corpus.diffs.download_diff import download_diffs_for
 
-load_dotenv()
-api_key = os.getenv('S2_API_KEY')
-HEADERS = {"x-api-key": api_key}
-BASE_DIR = os.getenv('BASE_DIR')
 
-BASE_URL = "https://api.semanticscholar.org/datasets/v1/diffs"
 start_release_id = "2024-06-18"
 end_release_id = "2024-06-25"
 
