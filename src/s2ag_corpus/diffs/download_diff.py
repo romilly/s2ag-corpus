6f8d3c810827785fce3a101b8bbc33cbdeb926f3
Romilly Cocking <romilly.cocking@gmail.com>
2024-07-01 10:13:22 +0100

    Improved logging in diff downloader.

--- src/s2ag_corpus/diffs/download_diff.py
+++ src/s2ag_corpus/diffs/download_diff.py
@@ -2,13 +2,16 @@ import requests
 import os
 from dotenv import load_dotenv
 
+from s2ag_corpus.monitor import Monitor
+
 load_dotenv()
 api_key = os.getenv('S2_API_KEY')
 HEADERS = {"x-api-key": api_key}
 BASE_DIR = os.getenv('BASE_DIR')
 BASE_URL = "https://api.semanticscholar.org/datasets/v1/diffs"
 
-def download_diffs_for(start_release_id: str, end_release_id: str, dataset_name: str):
+
+def download_diffs_for(start_release_id: str, end_release_id: str, dataset_name: str, monitor: Monitor):
     url = f"{BASE_URL}/{start_release_id}/to/{end_release_id}/{dataset_name}"
     response = requests.get(url, headers=HEADERS)
     diffs = response.json()['diffs']
@@ -17,16 +20,17 @@ def download_diffs_for(start_release_id: str, end_release_id: str, dataset_name:
     for diff in diffs:
         for file_type in ['update_files', 'delete_files']:
             links = diff[file_type]
+            monitor.info(f"downloading {file_type}: {len(links)} files")
             for (index, link) in enumerate(links):
                 response = requests.get(link, headers=HEADERS)
                 file_name = f"{file_type}-{index:03}.gz"
                 file_path = f"{diff_dir}/{file_name}"
                 if os.path.exists(file_path):
-                    print(f"Skipping {file_name}")
+                    monitor.info(f"Skipping {file_name}")
                     continue
                 with open(file_path,"wb") as uf:
                     uf.write(response.content)
-                    print(f'writing {file_name}')
+                    monitor.info(f'writing {file_name}')
 
 
 
