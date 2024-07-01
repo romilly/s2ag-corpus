faafe8423718c5695859d6e701f29c66cd025e02
Romilly Cocking <romilly.cocking@gmail.com>
2024-07-01 17:25:04 +0100

    Progressing production run of diffs: next, citations.

---
+++ src/apply-diffs-for-citations.py
@@ -0,0 +1,8 @@
+from s2ag_corpus.diffs.apply_production_diffs import apply_diffs_for_dataset
+
+dataset_name = 'citations'
+release_id = '2024-06-25'
+
+apply_diffs_for_dataset(dataset_name, release_id)
+
+
