8dc860dba05634447fd41b752af5d8eca7e76b7f
Romilly Cocking <romilly.cocking@gmail.com>
2024-07-01 10:38:24 +0100

    Preparing for production run.

--- src/apply-diffs-for-paper-ids.py
+++ src/apply-diffs-for-paper-ids.py
@@ -1,5 +1,7 @@
 import os
 
+from dotenv import load_dotenv
+
 from s2ag_corpus.database_catalogue import production_connection
 from s2ag_corpus.diffs.apply_diffs import ApplyDiffs
 from s2ag_corpus.logging_monitor import LoggingMonitor
@@ -8,6 +10,8 @@ dataset_name = 'paper-ids'
 release_id = '2024-06-25'
 connection = production_connection()
 monitor = LoggingMonitor()
+
+load_dotenv()
 BASE_DIR = os.getenv('BASE_DIR')
 diff_directory = os.path.join(BASE_DIR, 'diffs')
 
