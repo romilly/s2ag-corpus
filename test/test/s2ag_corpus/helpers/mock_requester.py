f06a5049a0fb013fceefe6a6427af271bb175e49
Romilly Cocking <romilly.cocking@gmail.com>
2024-07-08 10:49:41 +0100

    About to replace MockAPI.

--- test/test/s2ag_corpus/helpers/mock_requester.py
+++ test/test/s2ag_corpus/helpers/mock_requester.py
@@ -5,17 +5,9 @@ from s2ag_corpus.requester.requester import Requester
 
 
 class MockRequester(Requester):
+    def post(self, url: str, ids: dict) -> dict:
+        pass
+
     def get(self, url: str, parameters: Optional[str] = None) -> dict:
         pass
 
-    def get_citation_items(self):
-        pass
-
-    def __init__(self):
-        self.papers = {}
-
-    def add_paper(self, pid: str, text: str):
-        self.papers[pid] = text
-
-    def get_paper_json(self, pid) -> dict:
-        return json.loads(self.papers[pid])
